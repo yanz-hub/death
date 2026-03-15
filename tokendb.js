61a8519bd12baaca7d1b1d1ac7e63616dc8ca907
Yanxzz-Not-Dev <noeryoan2@gmail.com>
2026-03-15 07:16:12 +0700

    Update token list

--- tokendb.js
+++ tokendb.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8597407493:AAHdPRQNWffpuxewXQ0XzMxsa4uagWW44kg",
-    "8374847196:AAHpqqk8nsoZuiQoHm8KxR3T8BzzNZg_Y4U"
+    "8374847196:AAHpqqk8nsoZuiQoHm8KxR3T8BzzNZg_Y4U",
+    "8147463559:AAH-IZ6Rh7MGb3tmx882qJY0XrazugF9Sok"
   ]
 }
